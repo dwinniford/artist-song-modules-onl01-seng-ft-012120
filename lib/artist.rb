331a7393b649a00effbb7a7d94d1ae40c731dc4e
Dave Winniford <dwinniford@gmail.com>
2020-01-30 13:51:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,13 +3,14 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
-  extend Memorable, Findable
-  include Paramble
+  extend Memorable::ClassMethods, Findable
+  include Paramble::InstanceMethods
 
   @@artists = []
 
   def initialize
     #self.class.all << self
+    super
     @songs = []
   end
 
